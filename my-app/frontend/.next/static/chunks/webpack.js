26799e728c86954c7433bb8c21975cd7d71f8142
joao <jrbotica10@gmail.com>
2024-04-21 19:25:19 +0100

    equipment backend done

--- my-app/frontend/.next/static/chunks/webpack.js
+++ my-app/frontend/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "d07f576edaa68397"; }
+/******/ 		__webpack_require__.h = function() { return "2221e92b39bae3a3"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
